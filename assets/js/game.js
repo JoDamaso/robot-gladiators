e05a75b9a55ee030b6e3d22322e92c1eb431a1d8
JoDamaso <darryldamaso@gmail.com>
2022-01-05 11:46:34 -0600

    loop for bots fixed

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,7 +11,8 @@ var enemyAttack = 12;
 
 var fight = function(enemyNames) {
     ///fight function statements
-    window.alert("Welcome to Robot Galdiators!");
+    // window.alert("Welcome to Robot Galdiators!");
+    while(enemyHealth > 0) {
     // Alert players that they are starting the round
     //if player chooses fight 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -58,12 +59,14 @@ var fight = function(enemyNames) {
     } else {
         window.alert("You need to choose a valid option. Try again!");
     }
+    };
 };
 
-// fight();
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
 
 
